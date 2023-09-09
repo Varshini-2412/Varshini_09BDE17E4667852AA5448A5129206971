a4bcae4e97bd4844b9dae7cb8e6fba5c754dd277
Varshini-2412 <144543248+Varshini-2412@users.noreply.github.com>
2023-09-09 20:57:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+num=int(input("please enter the number your wish:"))
+if(num%4==0):
+  if(num%100==0):
+    if(num%400==0):
+      print("%d is a leap year"%num)
+    else:
+      print("%d is not"%num)
+  else:
+    print("%d is aleap year"%num)
+else:
+  print("%d is not"%num )
+        
+         
+    
